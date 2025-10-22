ef300aee1eed8e1c214fc702ae74f98ea3b0a1ec
Navneet <navneetsingh17736@gmail.com>
2025-10-22 19:17:10 -0400

    reduced bundle

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,4 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
@@ -11,7 +8,6 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
   ],
